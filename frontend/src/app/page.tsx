ae04e23664da89ecadedf5bf117fb7ca87235e70
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-02-24 18:34:34 +0200

    Remove redundant map component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Map } from "@/components/map";
 import { cn } from "@/utils/shadcn";
-import { Loader2Icon, LoaderIcon } from "lucide-react";
+import { Loader2Icon } from "lucide-react";
+import Map from "react-map-gl";
 import { useState } from "react";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function Home() {
   const [mapLoaded, setMapLoaded] = useState(false);
@@ -19,7 +21,17 @@ export default function Home() {
         <p>Recycler</p>
       </header>
       <main>
-        <Map onLoad={() => setMapLoaded(true)} />
+        <Map
+          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+          initialViewState={{
+            longitude: 27.678117958246627,
+            latitude: 62.892607388617456,
+            zoom: 14,
+          }}
+          onLoad={() => setMapLoaded(true)}
+          style={{ background: "#424bb3ff", width: "100vw", height: "100vh" }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
         {!mapLoaded && (
           <div className="fixed flex inset-0 items-center justify-center flex-col gap-6 text-white">
             <Loader2Icon className="animate-spin" />
