9dd6063d901da8640de57804026376a4e2e4a934
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-03-27 20:38:06 +0200

    Add logo png

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,8 +4,10 @@ import { getCollectionSpots } from "@/services/api";
 import { cn } from "@/utils/shadcn";
 import { Loader2Icon } from "lucide-react";
 import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import Map, { CircleLayer, Layer, Source, SymbolLayer } from "react-map-gl";
+import logo from "./recycler-logo.png";
 
 const layerStyle: CircleLayer = {
   id: "point",
@@ -77,11 +79,11 @@ export default function Home() {
     <>
       <header
         className={cn(
-          "hidden fixed px-4 py-4 w-full top-0 z-50 items-center text-2xl text-black uppercase",
+          "hidden fixed px-4 py-2 w-full top-0 z-50 items-center text-2xl text-black uppercase",
           { flex: mapLoaded }
         )}
       >
-        <p>Recycler</p>
+        <Image src={logo} alt="Recycler logo" width={150} />
       </header>
       <main>
         <Map
