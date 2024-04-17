6b1d13bfe8df750a49ca8bcab34164feeee49660
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-04-17 10:21:20 +0300

    Add alert to custom control

--- frontend/src/components/control-scaffolding.tsx
+++ frontend/src/components/control-scaffolding.tsx
@@ -2,12 +2,14 @@ import { useControl } from "react-map-gl";
 
 class ControlScaffoldingClass {
   [x: string]: any;
+
   onAdd(map: any) {
     this._map = map;
     this._container = document.createElement("button");
     this._container.className =
       "mapboxgl-ctrl border border-gray-400 bg-white text-black px-2 py-1 rounded-md";
     this._container.textContent = "Custom control";
+    this._container.onclick = () => alert("Button clicked");
     return this._container;
   }
 
