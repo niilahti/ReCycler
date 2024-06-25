816955e01216bf4e932b5fb32d67712a3671733e
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-06-25 08:43:03 +0300

    Use Docker for infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "beanstalk_role" {
 
 resource "aws_iam_role_policy_attachment" "beanstalk_role_policy" {
   role       = aws_iam_role.beanstalk_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
 }
 
 resource "aws_iam_instance_profile" "beanstalk_profile" {
@@ -27,19 +27,28 @@ resource "aws_iam_instance_profile" "beanstalk_profile" {
   role = aws_iam_role.beanstalk_role.name
 }
 
-resource "aws_s3_bucket" "recycler_app" {
-  bucket = "recycler-app"
+resource "aws_ecr_repository" "my_ecr_repository" {
+  name                 = "recycler-api"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.my_ecr_repository.repository_url
 }
 
 resource "aws_elastic_beanstalk_application" "app" {
-  name        = "recycler-app-api"
-  description = "Recycler API"
+  name        = "recycler-app"
+  description = "Recycler App"
 }
 
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = "recyler-api-env"
+  name                = "recycler-app-env"
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2023 v4.1.0 running Python 3.11" # Adjust based on your application
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.9 running ECS"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -60,59 +69,47 @@ resource "aws_elastic_beanstalk_environment" "env" {
   }
 
   # Pass RDS connection information as environment variables
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "POSTGRES_HOST"
-    value     = aws_db_instance.default.address
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:application:environment"
+  #   name      = "POSTGRES_HOST"
+  #   value     = aws_db_instance.default.address
+  # }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "POSTGRES_PORT"
-    value     = "5432"
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:application:environment"
+  #   name      = "POSTGRES_PORT"
+  #   value     = "5432"
+  # }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "POSTGRES_DB"
-    value     = aws_db_instance.default.db_name
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:application:environment"
+  #   name      = "POSTGRES_DB"
+  #   value     = aws_db_instance.default.db_name
+  # }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "POSTGRES_USER"
-    value     = aws_db_instance.default.username
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:application:environment"
+  #   name      = "POSTGRES_USER"
+  #   value     = aws_db_instance.default.username
+  # }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "POSTGRES_PASSWORD"
-    value     = aws_db_instance.default.password
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:application:environment"
+  #   name      = "POSTGRES_PASSWORD"
+  #   value     = aws_db_instance.default.password
+  # }
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage   = 20
-  db_name             = "recyclerdb"
-  engine              = "postgres"
-  instance_class      = "db.t3.micro"
-  password            = var.db_password
-  publicly_accessible = true
-  username            = var.db_user
+# resource "aws_db_instance" "default" {
+#   allocated_storage   = 20
+#   db_name             = "recyclerdb"
+#   engine              = "postgres"
+#   instance_class      = "db.t3.micro"
+#   publicly_accessible = true
+#   username            = "recycler_user"
+#   skip_final_snapshot = true
 
-  tags = {
-    Name = "RecyclerDbInstance"
-  }
-}
-
-variable "db_user" {
-  description = "The username for the RDS instance"
-  type        = string
-  default     = "postgres_user"
-}
-
-variable "db_password" {
-  description = "The password for the RDS instance"
-  type        = string
-  default     = "postgres_password"
-}
+#   tags = {
+#     Name = "RecyclerDbInstance"
+#   }
+# }
