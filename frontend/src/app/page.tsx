2f4c395e25e9c2eddeabd0fc20fee1363ad31737
Jussi <jussi@mindmote.fi>
2024-06-26 16:11:55 +0300

    editing landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,18 +4,18 @@ import Container from "@/components/container";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import hero from "./recycle.png";
+//import hero from "./recycle.png"; //let´s place when suitable photo
 
 const HomePage = () => {
   return (
     <>
-      <div className="h-48 bg-gray-400 w-full border-b border-b-gray-400">
+      {/*<div className="h-48 bg-gray-400 w-full border-b border-b-gray-400">
         <Image
           alt=""
           className="object-cover w-full h-full object-center"
           src={hero}
-        />
       </div>
+         */}
       <Container>
         <h1 className="text-xl font-medium mb-4 font-sans">Tervetuloa Recycleriin</h1>
         <p className="mb-8 font-sans">
